46a44dd762c4d7c6351a2305c2400fbca2a0b625
spriggsc <carynspriggs@gmail.com>
2015-07-06 22:57:40 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var x = Number (prompt('Please enter a number', 10));
+console.log(x);
+   
+var y = Number (prompt('Please enter a number', 10));
+console.log(y);
+   
+if(isNaN(x) || isNaN(y)){
+  alert('Please enter a number!!!!!');
+}else{
+  alert(x+y);
+  console.log(x + y);
+}
+  
